a3ae6bb65670fa6ead73dd851c955125241af30b
jvsena42 <jvsena16@gmail.com>
2019-10-31 22:19:01 -0300

    Options menu

---
+++ core/src/scenes/Options.java
@@ -0,0 +1,78 @@
+package scenes;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.jvsena42.jackthegiant.GameMain;
+
+import helpers.GameInfo;
+
+public class Options implements Screen {
+
+    private GameMain game;
+
+    private OrthographicCamera mainCamera;
+    private Viewport gameViewport;
+
+    private Texture bg;
+
+    public Options(GameMain game) {
+        this.game = game;
+
+        mainCamera = new OrthographicCamera();
+        mainCamera.setToOrtho(false, GameInfo.WIDTH,GameInfo.HEIGH);
+        mainCamera.position.set(GameInfo.WIDTH/2f,GameInfo.HEIGH/2f,0);
+
+        gameViewport = new StretchViewport(GameInfo.WIDTH,GameInfo.HEIGH,mainCamera);
+
+        bg = new Texture("Backgrounds/Options BG.png");
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.getBatch().begin();
+
+        game.getBatch().draw(bg,0,0);
+
+        game.getBatch().end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gameViewport.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
