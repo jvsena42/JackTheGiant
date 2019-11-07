13146a6e57e178684dd069d4cad27dd328d51c47
jvsena42 <jvsena16@gmail.com>
2019-11-06 21:44:28 -0300

    Collectables

---
+++ core/src/collectables/Collectable.java
@@ -0,0 +1,51 @@
+package collectables;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+import helpers.GameInfo;
+
+public class Collectable extends Sprite {
+
+    private World world;
+    private Body body;
+    private String name;
+    private Fixture fixture;
+
+    public Collectable(World world, String name) {
+        super(new Texture( "Collectables/ "+name+".png"));
+        this.world = world;
+        this.name = name;
+    }
+
+    public void createCollectableBody(){
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+
+        bodyDef.position.set((getX()-getWidth()/2f)/ GameInfo.PPM,(getY()-getHeight()/2f)/GameInfo.PPM);
+
+        body = world.createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox((getWidth()/2)/GameInfo.PPM,(getHeight()/2)/GameInfo.PPM);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+
+        fixture = body.createFixture(fixtureDef);
+
+        shape.dispose();
+    }
+
+    public void setCollectablePosition (float x, float y){
+        setPosition(x,y);
+        createCollectableBody();
+    }
+}
